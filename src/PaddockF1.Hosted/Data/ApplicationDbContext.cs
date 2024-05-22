f79fd58f371dda4e2ab2e4d55c3c9d15e1b00660
Théo Willemyns <willemynstheo@gmail.com>
2024-05-22 20:55:00 +0200

    Changements CSS pages d'authentification

--- src/PaddockF1.Hosted/Data/ApplicationDbContext.cs
+++ src/PaddockF1.Hosted/Data/ApplicationDbContext.cs
@@ -5,5 +5,15 @@ namespace PaddockF1.Hosted.Data
 {
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
     {
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            
+            builder.Entity<ApplicationUser>(entity =>
+            {
+                entity.Ignore(m => m.PhoneNumber);
+                entity.Ignore(m => m.PhoneNumberConfirmed);
+            });
+        }
     }
 }
--- src/PaddockF1.Hosted/Program.cs
+++ src/PaddockF1.Hosted/Program.cs
@@ -25,7 +25,7 @@ builder.Services.AddAuthentication(options =>
     })
     .AddIdentityCookies();
 
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+var connectionString = builder.Configuration.GetConnectionString("SQLite") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlite(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
