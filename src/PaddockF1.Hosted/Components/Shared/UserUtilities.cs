3cd3e0f3dd61e40a005d7d4899be598f509efe90
twillemyns <twillemyns@bils-deroo.fr>
2024-06-13 16:06:56 +0200

    Création classe méthodes beaucoup utilisées

---
+++ src/PaddockF1.Hosted/Components/Shared/UserUtilities.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using PaddockF1.Hosted.Data;
+using PaddockF1.Hosted.Data.Models;
+
+namespace PaddockF1.Hosted.Components.Shared;
+
+public class UserUtilities(
+    AuthenticationStateProvider authStateProvider,
+    ApplicationService applicationService)
+{
+    public async Task<ApplicationUser?> GetCurrentUser()
+    {
+        var authenticationState = await authStateProvider.GetAuthenticationStateAsync();
+        var user = authenticationState.User;
+        var userId = user.FindFirst(u => u.Type.Contains("nameidentifier"))?.Value;
+
+        return userId is null ? null : applicationService.GetUserById(userId);
+    }
+}
