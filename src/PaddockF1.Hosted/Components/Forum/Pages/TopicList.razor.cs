36c9871dd81202ff1e19179aaf609f691f870a5a
twillemyns <twillemyns@bils-deroo.fr>
2024-06-13 16:08:55 +0200

    Ajout formulaire création topic

--- src/PaddockF1.Hosted/Components/Forum/Pages/TopicList.razor.cs
+++ src/PaddockF1.Hosted/Components/Forum/Pages/TopicList.razor.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using PaddockF1.Hosted.Components.Shared;
 using PaddockF1.Hosted.Data;
 using PaddockF1.Hosted.Data.Models;
 
@@ -7,11 +10,56 @@ namespace PaddockF1.Hosted.Components.Forum.Pages;
 public partial class TopicList : ComponentBase
 {
     [Inject] private ApplicationService ApplicationService { get; set; } = default!;
-    
+
+    [Inject] private NavigationManager NavigationManager { get; set; } = default!;
+
+    [Inject] private AuthenticationStateProvider AuthenticationStateProvider { get; set; } = default!;
+
+    [SupplyParameterFromForm] public InputModel NewTopic { get; set; } = new();
+
     private List<Topic> _topics = default!;
-    
+
+    private ApplicationUser? _user;
+
     protected override void OnInitialized()
     {
         _topics = ApplicationService.GetTopics().ToList();
     }
+
+    protected override async Task OnInitializedAsync()
+    {
+        var userUtilities = new UserUtilities(AuthenticationStateProvider, ApplicationService);
+
+        _user = await userUtilities.GetCurrentUser();
+    }
+
+    private void CreateTopic()
+    {
+        if (_user is null)
+        {
+            NavigationManager.NavigateTo("/Account/login");
+            return;
+        }
+
+        var topic = new Topic
+        {
+            Title = NewTopic.Title,
+            Description = NewTopic.Description,
+            Author = _user
+        };
+        ApplicationService.AddTopic(topic);
+
+        NavigationManager.NavigateTo($"/forum/{topic.Id}");
+    }
+}
+
+public class InputModel
+{
+    [Required(ErrorMessage = "Le titre est obligatoire.")]
+    [StringLength(50, MinimumLength = 3, ErrorMessage = "Le titre doit contenir entre 3 et 50 caractères.")]
+    public string Title { get; set; } = default!;
+
+    [Required(ErrorMessage = "La description est obligatoire.")]
+    [StringLength(250, MinimumLength = 5, ErrorMessage = "La description doit contenir entre 5 et 250 caractères.")]
+    public string Description { get; set; } = default!;
 }
