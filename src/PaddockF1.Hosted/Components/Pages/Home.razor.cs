b1f096d118b85f59f6c16761eaae06f68653bf3c
twillemyns <twillemyns@bils-deroo.fr>
2024-06-14 16:16:39 +0200

    Intégration forum dans page d'accueil

---
+++ src/PaddockF1.Hosted/Components/Pages/Home.razor.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+using PaddockF1.Hosted.Data;
+using PaddockF1.Hosted.Data.Models;
+
+namespace PaddockF1.Hosted.Components.Pages;
+
+public partial class Home
+{
+    [Inject] private ApplicationService ApplicationService { get; set; } = default!;
+    
+    private IEnumerable<Topic> Topics { get; set; } = default!;
+
+    protected override void OnInitialized()
+    {
+        Topics = ApplicationService.GetTopics().OrderBy(x => x.CreatedAt).Reverse().Take(3);
+    }
+}
